7bc806f999cdb8ff49a0b917e8079adabb401726
sanjaypbce <www.sanjayijk07@gmail.com>
2025-01-11 18:06:55 +0530

    Fix: Set base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/realTimeChat-MERN/",
+  plugins: [react()],
+  base: "/realTimeChat-MERN/", // Use your repository name here
 });
